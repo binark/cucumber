93a36a71a9c249d4d8fb4ef48b9940a322f18a7d
kenany <armelknyobe@gmail.com>
2024-06-30 17:26:31 +0200

    Setup cucumber report

--- testing-project/src/test/java/com/binark/testingproject/TestSetup.java
+++ testing-project/src/test/java/com/binark/testingproject/TestSetup.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources")
+@CucumberOptions(features = "src/test/resources",
+        plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber/report.json"},
+        monochrome = true)
 public class TestSetup {
 
 }
